205db4be4a0a9c2f274b7a74d40b58c49feb7658
jayasreekomaripalam-netizen148 <jayasreekomaripalam@gmail.com>
2026-09-11 11:51:04 +0530

    Initialize database for production deployment

--- app.py
+++ app.py
@@ -236,15 +236,17 @@ def messages():
     ])
 
 
+# ================= DATABASE INITIALIZATION =================
+
+init_database()
+
+
 # ================= START SERVER =================
 
 if __name__ == "__main__":
-
-    init_database()
 
     app.run(
         host="0.0.0.0",
         port=5000,
         debug=True
     )
-
